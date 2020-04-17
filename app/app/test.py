8a557be168ab250da372871a0e9c0be332f36d94
Akhlaq Ahmad <akhlaq.ahmad@kiwitech.com>
2020-04-18 00:49:17 +0530

    Addded a UNIT test

--- app/app/test.py
+++ app/app/test.py
@@ -4,4 +4,4 @@ from app.calc import add
 class CalcTests(TestCase):
     def test_add_numbers(self):
         """ test two numbers adedd together """
-        self.assertEqual(add(3,8), 11)
+        self.assertEqual(add(3, 8), 11)
